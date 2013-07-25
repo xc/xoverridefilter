86ced66489aea2709aba32cf7382ab14f38956da
Chen Xiongjie <xc@aplia.no>
2013-07-25 13:02:42 +0200

    Updated: event name

--- settings/site.ini.append.php
+++ settings/site.ini.append.php
@@ -1,6 +1,6 @@
 <?php /* #?ini charset="utf-8"?
 
 [Event]
-Listeners[]=nodeview/pre_rending@xOverrideConditionFilter::filter
+Listeners[]=content/pre_rending@xOverrideConditionFilter::filter
 
 */ ?>
