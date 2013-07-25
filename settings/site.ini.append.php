75923e01217d88950f203910cabbeff54b9ab19c
Chen Xiongjie <chen.xcmail@gmail.com>
2013-07-25 15:58:08 +0200

    typo

--- settings/site.ini.append.php
+++ settings/site.ini.append.php
@@ -1,6 +1,6 @@
 <?php /* #?ini charset="utf-8"?
 
 [Event]
-Listeners[]=content/pre_rending@xOverrideConditionFilter::filter
+Listeners[]=content/pre_rendering@xOverrideConditionFilter::filter
 
 */ ?>
